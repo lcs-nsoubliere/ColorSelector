bd4f08839d6d7bafa78b7dfed8671284482b5bef
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-10-27 12:03:38 -0400

    Now shoes 3 monochromatic colours with it called dark color and darker color

--- ColorSelector/ContentView.swift
+++ ColorSelector/ContentView.swift
@@ -15,29 +15,70 @@ struct ContentView: View {
     
     //MARK: Computed properties
     
+    //The selected hue express as a value betwene 0 and 1.0
+    private var hue: Double {
+        return selectedHue / 360.0
+    }
+    
+    //Make the color that SwiftUI will use to set the background of the color swatch
+    private var baseColor: Color {
+        return Color(hue: hue, saturation: 0.8, brightness: 0.9)
+    }
+ 
+    private var darkColor: Color {
+        return Color(hue: hue, saturation: 0.8, brightness: 0.7)
+    }
+    
+    private var darkerColor: Color {
+        return Color(hue: hue, saturation: 0.8, brightness: 0.5)
+    }
+
+    
     //Interface
     var body: some View {
         VStack {
+            
             VStack{
-                
+
                 
             }
             .frame(width: 200, height: 200)
-            .background(Color.blue)
+            .background(baseColor)
             
             Text("Hue")
                 .bold()
-            Text("\(selectedHue)°")
+            Text("\(selectedHue.formatted(.number.precision(.fractionLength(1))))°")
             Slider(value: $selectedHue,
                    in: 0...360,
                    label: { Text("Hue")},
                    minimumValueLabel: {Text("0")},
                    maximumValueLabel: {Text("360")})
+ 
+            HStack{
+                
+                VStack{
+                    
+                }
+                .frame(width: 75, height: 75)
+                .background(baseColor)
+                
+                VStack{
+                    
+                }
+                .frame(width: 75, height: 75)
+                .background(darkColor)
+                
+                VStack{
+                    
+                }
+                .frame(width: 75, height: 75)
+                .background(darkerColor)
+            }
+
             
-            Spacer()
             
         }
-        .padding()
+     
     }
 }
 
