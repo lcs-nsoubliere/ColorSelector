2b1283c69b1ec39b1696449a7265964f4a1da1bc
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-10-27 11:15:12 -0400

    Initial Commit

---
+++ ColorSelector/ColorSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSelectorApp.swift
+//  ColorSelector
+//
+//  Created by Noah Alexandre Soubliere on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct ColorSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
